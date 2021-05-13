6f993c59b3442790190440c4fd757d3d46cf0978
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-05-13 13:21:16 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -13,15 +13,25 @@ const expTable=document.querySelector("#expenseTable");
 //get total span elemt
 const totalSpan=document.querySelector("#total");
 
+const demo=document.querySelector("#demo");
+
+const editInput=document.querySelector("#editInput");
+
 //initilize total to 0
 let totalExp=0;
 
 //make empty array of expense
 let allExpense=[];
 
+function getInput()
+{
+    
+}
+
 //function to add expense to array
 function addAmount()
 {
+    
     //get amtText value
     const amtValue=amtText.value;
     // convert to number format
@@ -47,6 +57,7 @@ function addAmount()
     
     displayExpense(allExpense);
     
+    console.log(expenseItem.id);
 
     //reseting input feilds
     amtText.value="";
@@ -84,7 +95,7 @@ function getDataString(now)
 //set total 
 function setTotal(newTotal)
 {
-    totalSpan.innerHTML=newTotal;
+    totalSpan.textContent=newTotal;
 }
 //delete item 
 function deteleItem(clickedDateString)
@@ -102,8 +113,93 @@ function deteleItem(clickedDateString)
 
    allExpense=newArr;
    displayExpense(allExpense);
+}
+
+function editItem(clickedDateString)
+{
+    var editAmt="99";
+    let editDesp="Sushant";
+    let editDate="";
+    let editMOm="";
+    console.log("edit called",clickedDateString);
+   // editExpItem={};
+    for(let i=0;i<allExpense.length;i++)
+    {
+        if(allExpense[i].dateString===clickedDateString)
+        {
+            editAmt=Number(allExpense[i].amt);
+            editDesp=allExpense[i].desp;
+            editMom=allExpense[i].mom;
+            editDateString=allExpense[i].dateString ;
+          //  expText.innerHTML=allExpense[i].desp;
+        }
+    }
+    console.log(editAmt,editDesp,"changed?");
+   /// expText.innerHTML=editAmt;
 
 
+
+    const inputs= `
+    <li id="{$id}" class="list-group-item d-flex justify-content-between">
+    <div class="d-flex flex-column">
+        <input id="editDesp" type="text" value="${editDesp}" placeholder="spent On">
+        <small id="editDate" class="text-muted" value="${editMom}">${editMom}</small>
+    </div>
+    <div>
+        <span class="px-5">
+            <input id="editAmtInput" type="text" value="${editAmt}" placeholder="Amount spent">
+        </span>
+        <button type="button" class="btn btn-outline-primary btn-sm" onclick="{addEditAmount()}">
+            <i class="fas fa-edit"></i>
+        </button>
+    </div>
+</li>`
+    editInput.innerHTML=inputs;
+    deteleItem(clickedDateString);
+   // editBlockDisplay("hidden");
+   
+
+}
+
+function addEditAmount()
+{   
+    editedItem={};
+    console.log("edit adding started")
+    const toEditAmt=parseInt(editAmtInput.value,10);
+    //console.log({toEditAmt});
+    const toEditDesp=editDesp.value;
+    //console.log({toEditDesp});
+    //const toEditID=ed
+    editedItem.amt=toEditAmt;
+    editedItem.desp=toEditDesp;
+    console.log(editedItem);
+    allExpense.push(editedItem);
+    displayExpense(allExpense);
+    //editBlockDisplay("block");
+}
+
+
+function editBlockDisplay(status)
+{
+    if(status==="block")
+    {
+        editInput.style.visibility = "hidden";
+        console.log("edit block hidden");
+    }
+    else if(status==="hidden")
+    {
+        editInput.style.visibility = "block";
+        console.log("edit block shown");
+    }
+
+}
+
+function inputValue()
+{
+    var editAmtValue=prompt("enter amt");
+    //const editAmt=parseInt(editAmtValue,10);
+    //console.log(Object.prototype.toString.call(editAmt),"input amt");
+    return editAmtValue;
 }
 
 //function to render list
@@ -111,7 +207,7 @@ function renderExpense(itemToPrint)
 {
     //totalAmt=totalAmt+itemToPrint.amt;
     return `
-    <li class="list-group-item d-flex justify-content-between">
+    <li id="${itemToPrint.id}" class="list-group-item d-flex justify-content-between">
     <div class="d-flex flex-column">
         ${itemToPrint.desp}
         <small class="text-muted">${itemToPrint.moment}</small>
@@ -120,6 +216,9 @@ function renderExpense(itemToPrint)
         <span class="px-5">
             ${itemToPrint.amt}
         </span>
+        <button type="button" class="btn btn-outline-primary btn-sm" onclick="editItem(${itemToPrint.dateString})">
+            <i class="fas fa-edit"></i>
+        </button>
         <button type="button" class="btn btn-outline-danger btn-sm" onclick="deteleItem(${itemToPrint.dateString})">
             <i class="fas fa-trash-alt"></i>
         </button>
